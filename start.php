0cc2a6fbcd83f695631afb5702bc90afb2b893b0
Bolot <bolotkalil@gmail.com>
2017-03-07 12:47:58 +0600

    re #3 settings

--- start.php
+++ start.php
@@ -1,25 +1,28 @@
 <?php
 
-//\Phpfox_Module::instance()
-//    ->addServiceNames([
-////        'gradeservice.process' => '\Apps\CM_GradeService\Service\Process',
-////        'gradeservice' => '\Apps\CM_GradeService\Service\GradeService',
-//    ])
-//    ->addComponentNames('controller', [
-////        'gradeservice.admincp.add-question' => 'Apps\CM_GradeService\Controller\Admin\AddQuestion',
-////        'gradeservice.admincp.questions' => 'Apps\CM_GradeService\Controller\Admin\Questions',
-////        'gradeservice.admincp.statistics' => 'Apps\CM_GradeService\Controller\Admin\Statistics',
-//    ])
-//    ->addComponentNames('block', [
-////        'gradeservice.statistics' =>  '\Apps\CM_GradeService\Block\Statistics'
-//    ])
-//    ->addAliasNames('cashpayment', 'CM_GradeService')
-//    ->addTemplateDirs([
-//        'cashpayment' => PHPFOX_DIR_SITE_APPS . 'CM_GradeService' . PHPFOX_DS . 'views',
-//    ]);
-//
+\Phpfox_Module::instance()
+    ->addServiceNames([
+        'cashpayment.settings' => '\Apps\CM_CashPayment\Service\Process',
+        'cashpayment' => '\Apps\CM_CashPayment\Service\cashpayment',
+    ])
+    ->addComponentNames('controller', [
+        'cashpayment.admincp.settings' => 'Apps\CM_CashPayment\Controller\Admin\Settings',
+    ])
+    ->addAliasNames('cashpayment', 'CM_CashPayment')
+    ->addTemplateDirs([
+        'cashpayment' => PHPFOX_DIR_SITE_APPS . 'CM_CashPayment' . PHPFOX_DS . 'views',
+    ]);
+
 //event('app_settings', function ($settings){
-//    if (isset($settings['cm_cashpayment_enabled'])) {
-//        \Phpfox::getService('admincp.module.process')->updateActivity('CM_GradeService', $settings['cm_cashpayment_enabled']);
+//    if (isset($settings['CM_CashPayment_enabled'])) {
+//        \Phpfox::getService('admincp.module.process')->updateActivity('CM_CashPayment', $settings['CM_CashPayment_enabled']);
 //    }
 //});
+
+group('/admincp/cashpayment/', function(){
+    route('settings', 'cashpayment.admincp.settings');
+});
+
+group('/cashpayment/', function(){
+    route('setting/save', 'cashpayment.admincp.settings');
+});
